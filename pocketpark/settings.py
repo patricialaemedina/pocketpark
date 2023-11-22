32cb2374a424ff22d4fb4064f5e7f65fa8b8b635
Patricia Medina <patriciamedina@Patricias-MacBook-Air.local>
2023-11-22 21:37:03 +0800

    init

--- pocketpark/settings.py
+++ pocketpark/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yag=+m+vq2gj+erj()f^t@_g+jf+(((_^he(7c)a8q$dg$yb*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ph.pocketpark.online']
 

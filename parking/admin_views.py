9ea89b3d4ec04341e0462b003897c186b3346eeb
Patricia Medina <patriciamedina@Patricias-MacBook-Air.local>
2024-01-16 21:03:32 +0800

    fixed signature line

--- parking/admin_views.py
+++ parking/admin_views.py
@@ -143,6 +143,7 @@ def generate_report(request):
             story.append(Spacer(1, 10))
             story.append(Paragraph(f"Total Revenue: PHP {total_revenue}\n\n\n", styles["Normal"]))
 
+            story.append(Spacer(1, 4))
             name = "Christian Aban"
             line = "________________________________"
             position = "Parking Manager"
@@ -442,6 +443,7 @@ def generate_report(request):
                 position = "Parking Manager"
                 place = "Greenfield District Mandaluyong"
 
+                story.append(Spacer(1, 4))
                 story.append(Paragraph(name, centered_style))        
                 story.append(Paragraph(line, centered_style))        
                 story.append(Paragraph(position, centered_style))        
